44a17b762b695caea5efaa165ff3ce0f1a391283
einherjer-uy <andres.sica@gmail.com>
2013-05-13 18:34:53 -0300

    hello world freemarker style

--- src/main/java/org/einherjer/HelloWorldFreemarkerStyle.java
+++ src/main/java/org/einherjer/HelloWorldFreemarkerStyle.java
@@ -1,11 +1,13 @@
 package org.einherjer;
 
+import java.io.IOException;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 public class HelloWorldFreemarkerStyle {
 
@@ -13,10 +15,20 @@ public class HelloWorldFreemarkerStyle {
         Configuration config = new Configuration();
         config.setClassForTemplateLoading(HelloWorldFreemarkerStyle.class, "/");
 
-        Template helloTemplate = config.getTemplate("hello.ftl");
-        StringWriter writter = new StringWritter();
-        Map<String, Object> freemarkerData = new HashMap<String, Object>();
-
+        try {
+            Template helloTemplate = config.getTemplate("hello.ftl");
+            StringWriter writer = new StringWriter();
+            Map<String, Object> freemarkerData = new HashMap<String, Object>();
+            freemarkerData.put("name", "Freemarker");
+            helloTemplate.process(freemarkerData, writer);
+            System.out.println(writer);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (TemplateException e) {
+            e.printStackTrace();
+        }
     }
 
 }
